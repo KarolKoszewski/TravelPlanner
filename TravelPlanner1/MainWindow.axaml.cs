29d149f1fe0539506e2db26fc2e697ffee94569b
karol <karol.koszewski@cyfroweszkoly.pl>
2025-05-30 11:36:46 +0200

    Robie caly wyglada formularza

--- TravelPlanner1/MainWindow.axaml.cs
+++ TravelPlanner1/MainWindow.axaml.cs
@@ -1,11 +1,52 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using System;
+using System.Collections.Generic;
 
-namespace TravelPlanner1;
-
-public partial class MainWindow : Window
+namespace TravelPlanner1
 {
-    public MainWindow()
+
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        private Dictionary<string, string> CountryImages = new()
+        {
+            { "Włochy", "Assets/italy.jpg" },
+            { "Japonia", "Assets/japan.jpg" },
+            { "Norwegia", "Assets/norway.jpg" },
+            { "USA", "Assets/usa.jpg" },
+            { "Francja", "Assets/france.jpg" }
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CountryComboBox_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(CityTextBox.Text))
+            {
+                if (CountryComboBox.SelectedItem is ComboBoxItem selected)
+                {
+                    if (CountryImages.TryGetValue(selected.Content.ToString()!, out string path))
+                    {
+                        CountryImage.Source = new Bitmap(path);
+                    }
+                }
+            }
+        }
+
+        private void AddCity_Click(object? sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void ShowDetails_Click(object? sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
